5f7cc7d68746e1aac908e3f5bb0a44f9b9fb38d5
yaroslavTsebro <yaroslav.cebro@gmail.com>
2024-10-07 21:22:20 +0300

    *auth middleware changes

---
+++ services/user/src/middlewares/auth.ts
@@ -0,0 +1,31 @@
+import { AuthorizationError } from '@packages/data/contracts/errors/authorization-error';
+import { TokenMissingError } from '@packages/data/contracts/errors/token-missing-error';
+import { IncomingMessage } from 'http';
+import { InvalidTokenError } from '@packages/data/contracts/errors/invalid-token-error';
+import { JwtService } from '../services/jwt';
+
+export interface AuthenticatedRequest extends IncomingMessage {
+  userId?: string;
+  body?: any;
+}
+
+export const authMiddleware = (tokenService: JwtService) => {
+  return (req: AuthenticatedRequest): void => {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+      throw new AuthorizationError('Authorization header missing.');
+    }
+
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+      throw new TokenMissingError('Token missing.');
+    }
+
+    try {
+      const decoded = tokenService.verifyToken(token) as { userId: string };
+      req.userId = decoded.userId;
+    } catch (err) {
+      throw new InvalidTokenError('Invalid token.');
+    }
+  };
+};
